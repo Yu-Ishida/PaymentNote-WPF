f25598fbd4e29be9412e5e897968b99d90112138
Method() <hanjuku_wakasama@yahoo.co.jp>
2018-10-13 17:53:54 +0900

    初版コミット
    プロジェクト作成と画面作成まで。

---
+++ PaymentNote/PaymentNote/General.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * 共用定数及びメソッド定義クラス 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace PaymentNote
+{
+    class General
+    {
+
+        /// <summary>
+        /// エラーメッセージを表示します。
+        /// </summary>
+        /// <param name="errMessage">表示するエラーメッセージ</param>
+        public static void showErrorDlg(string errMessage)
+        {
+            MessageBox.Show("以下のエラーが発生しました。\n" + errMessage, "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
---
+++ PaymentNote/PaymentNote/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PaymentNote
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 「名前をつけて保存」メニューを選択したときのイベント
+        /// </summary>
+        /// <param name="sender">イベント発生元オブジェクト</param>
+        /// <param name="e">イベント引数</param>
+        private void SaveAsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                // エラーが発生
+                General.showErrorDlg(ex.Message);
+            }
+            finally
+            { }
+        }
+
+        /// <summary>
+        /// 「終了」メニューを選択したときのイベント
+        /// </summary>
+        /// <param name="sender">イベント発生元オブジェクト</param>
+        /// <param name="e">イベント引数</param>
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // アプリケーションを終了する。
+                Application.Current.Shutdown();
+            }
+            catch(Exception ex)
+            {
+                // エラーが発生
+                General.showErrorDlg(ex.Message);
+            }
+            finally
+            { }
+        }
+
+        /// <summary>
+        /// 「バージョン情報」メニューを選択したときのイベント
+        /// </summary>
+        /// <param name="sender">イベント発生元オブジェクト</param>
+        /// <param name="e">イベント引数</param>
+        private void ShowVerInfoMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("簡単支出帳\n(c) 2018 Yu Ishida.", "バージョン情報", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch(Exception ex)
+            {
+                // エラーが発生
+                General.showErrorDlg(ex.Message);
+            }
+            finally
+            { }
+        }
+
+        /// <summary>
+        /// 「追加」ボタンを押下した時のイベント
+        /// </summary>
+        /// <param name="sender">イベント発生元オブジェクト</param>
+        /// <param name="e">イベント引数</param>
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                // エラーが発生
+                General.showErrorDlg(ex.Message);
+            }
+            finally
+            { }
+        }
+
+        /// <summary>
+        /// 「削除」ボタンを押下したときのイベント
+        /// </summary>
+        /// <param name="sender">イベント発生元オブジェクト</param>
+        /// <param name="e">イベント引数</param>
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                // エラーが発生
+                General.showErrorDlg(ex.Message);
+            }
+            finally { }
+        }
+    }
+}
